105dc18074169f8e72ecdfb8544802067d63e01b
Tianshu Shen <tinashen.tianshu@gmail.com>
2022-04-04 17:36:11 -0400

    Reformat hyperparameter search scripts

--- cluster_bash/hyperparam_search/ml10/run_ml10.sh
+++ cluster_bash/hyperparam_search/ml10/run_ml10.sh
@@ -1,34 +1,22 @@
 #!/usr/bin/env bash
 
-# Try with  baseline VAEs
-#sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=def-ssanner --mail-user=tina.shen@mail.utoronto.ca --mail-type=ALL VAEmultilayer1.sh
-#sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=def-ssanner --mail-user=tina.shen@mail.utoronto.ca --mail-type=ALL VAEmultilayer2.sh
-#sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=def-ssanner --mail-user=tina.shen@mail.utoronto.ca --mail-type=ALL VAEmultilayer3.sh
-#sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=def-ssanner --mail-user=tina.shen@mail.utoronto.ca --mail-type=ALL VAEmultilayer4.sh
+# Baseline VAE-CF
+sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=def-ssanner VAE_CF1.sh
+sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=def-ssanner VAE_CF2.sh
+sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=def-ssanner VAE_CF3.sh
+sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=def-ssanner VAE_CF4.sh
 
-# Try with different sample sizes
-#sbatch --nodes=1 --time=96:00:00 --mem=64G --cpus-per-task=4 --gres=gpu:1 --account=def-ssanner VAEcontrast_samples1.sh
-#sbatch --nodes=1 --time=96:00:00 --mem=64G --cpus-per-task=4 --gres=gpu:1 --account=def-ssanner --mail-user=tina.shen@mail.utoronto.ca --mail-type=ALL VAEcontrast_samples2.sh
-#sbatch --nodes=1 --time=96:00:00 --mem=64G --cpus-per-task=4 --gres=gpu:1 --account=def-ssanner --mail-user=tina.shen@mail.utoronto.ca --mail-type=ALL VAEcontrast_samples3.sh
-#sbatch --nodes=1 --time=96:00:00 --mem=64G --cpus-per-task=4 --gres=gpu:1 --account=def-ssanner --mail-user=tina.shen@mail.utoronto.ca --mail-type=ALL VAEcontrast_samples4.sh
-
-
-# Formal tuning
-sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner --mail-user=tina.shen@mail.utoronto.ca --mail-type=ALL VAEcontrast_tuning1.sh
-sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner --mail-user=tina.shen@mail.utoronto.ca --mail-type=ALL VAEcontrast_tuning2.sh
-sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner --mail-user=tina.shen@mail.utoronto.ca --mail-type=ALL VAEcontrast_tuning3.sh
-sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner --mail-user=tina.shen@mail.utoronto.ca --mail-type=ALL VAEcontrast_tuning4.sh
-sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner --mail-user=tina.shen@mail.utoronto.ca --mail-type=ALL VAEcontrast_tuning5.sh
-sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner --mail-user=tina.shen@mail.utoronto.ca --mail-type=ALL VAEcontrast_tuning6.sh
-sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner --mail-user=tina.shen@mail.utoronto.ca --mail-type=ALL VAEcontrast_tuning7.sh
-sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner --mail-user=tina.shen@mail.utoronto.ca --mail-type=ALL VAEcontrast_tuning8.sh
-sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner --mail-user=tina.shen@mail.utoronto.ca --mail-type=ALL VAEcontrast_tuning9.sh
-sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner --mail-user=tina.shen@mail.utoronto.ca --mail-type=ALL VAEcontrast_tuning10.sh
-sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner --mail-user=tina.shen@mail.utoronto.ca --mail-type=ALL VAEcontrast_tuning11.sh
-sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner --mail-user=tina.shen@mail.utoronto.ca --mail-type=ALL VAEcontrast_tuning12.sh
-
-#sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner --mail-user=tina.shen@mail.utoronto.ca --mail-type=ALL VAEcontrast_finetuning1.sh
-
-# Try with different temperature tau
-
+# DCE-VAE tuning
+sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner DCE_VAE1.sh
+sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner DCE_VAE2.sh
+sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner DCE_VAE3.sh
+sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner DCE_VAE4.sh
+sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner DCE_VAE5.sh
+sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner DCE_VAE6.sh
+sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner DCE_VAE7.sh
+sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner DCE_VAE8.sh
+sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner DCE_VAE9.sh
+sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner DCE_VAE10.sh
+sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner DCE_VAE11.sh
+sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner DCE_VAE12.sh
 
--- cluster_bash/hyperparam_search/yelp/run_yelp.sh
+++ cluster_bash/hyperparam_search/yelp/run_yelp.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
-# baseline VAE model
-#sbatch --nodes=1 --time=48:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner VAEmultilayer1.sh  #invalid learning rate
-#sbatch --nodes=1 --time=48:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner VAEmultilayer2.sh
-#sbatch --nodes=1 --time=48:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner VAEmultilayer3.sh
-#sbatch --nodes=1 --time=48:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner VAEmultilayer4.sh
+# baseline VAE-CF model
+sbatch --nodes=1 --time=48:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner VAE_CF1.sh  #invalid learning rate
+sbatch --nodes=1 --time=48:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner VAE_CF2.sh
+sbatch --nodes=1 --time=48:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner VAE_CF3.sh
+sbatch --nodes=1 --time=48:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner VAE_CF4.sh
 
 # DCE-VAE model
-sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner --mail-user=tina.shen@mail.utoronto.ca --mail-type=ALL VAEcontrast_tuning1.sh
-sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner --mail-user=tina.shen@mail.utoronto.ca --mail-type=ALL VAEcontrast_tuning2.sh
-sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner --mail-user=tina.shen@mail.utoronto.ca --mail-type=ALL VAEcontrast_tuning3.sh
-sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner --mail-user=tina.shen@mail.utoronto.ca --mail-type=ALL VAEcontrast_tuning4.sh
-sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner --mail-user=tina.shen@mail.utoronto.ca --mail-type=ALL VAEcontrast_tuning5.sh
-sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner --mail-user=tina.shen@mail.utoronto.ca --mail-type=ALL VAEcontrast_tuning6.sh
+sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner DCE_VAE1.sh
+sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner DCE_VAE2.sh
+sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner DCE_VAE3.sh
+sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner DCE_VAE4.sh
+sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner DCE_VAE5.sh
+sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner DCE_VAE6.sh
 
