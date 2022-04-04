5d27aa5729094dc2ed5ebc93c5acd764054dc7f2
Tianshu Shen <tinashen.tianshu@gmail.com>
2022-04-04 16:58:17 -0400

    Format model saving folder and related files

--- cluster_bash/save_model/ml10/VAE_CF.sh
+++ cluster_bash/save_model/ml10/VAE_CF.sh
@@ -4,10 +4,6 @@ source ~/vae_uncertainty/bin/activate
 
 cd /home/tinashen/projects/def-ssanner/tinashen/VAE_uncertainty_word_embeddings
 
-# VAEmultilayer
-#python model_save.py --model_name VAEmultilayer --data_name ml10 --data_dir fold0 --conf VAE_multilayer.config --log_dir VAE_multilayer --top_items 15 --top_users 8000
-
-# VAEmultilayer1
 python model_save.py --model_name VAEmultilayer --data_name ml10_SIGIR --data_dir fold0 --conf VAE_multilayer1.config --log_dir VAE_multilayer1 --top_items 15 --top_users 8000
 
 
--- cluster_bash/save_model/ml10/run_ml10.sh
+++ cluster_bash/save_model/ml10/run_ml10.sh
@@ -1,20 +1,9 @@
 #!/usr/bin/env bash
 
-# VAE multilayer
+# VAE-CF
+sbatch --nodes=1 --time=01:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner --mail-user=tina.shen@mail.utoronto.ca --mail-type=ALL VAE_CF.sh
 
-#sbatch --nodes=1 --time=01:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner --mail-user=tina.shen@mail.utoronto.ca --mail-type=ALL VAEmultilayer.sh
-
-sbatch --nodes=1 --time=01:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner --mail-user=tina.shen@mail.utoronto.ca --mail-type=ALL VAEcontrast1.sh
-#sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner VAEmultilayer_contrast1.sh
-#sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner VAEcontrast3.sh
-#sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner VAEcontrast4.sh
-#sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner VAEcontrast5.sh
-#sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner VAEcontrast6.sh
-#sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner VAEcontrast7.sh
-#sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner VAEcontrast8.sh
-#sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner VAEcontrast9.sh
-#sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner VAEcontrast10.sh
-
+# DCE-VAE
 # this model has smaller number of positive samples
-#sbatch --nodes=1 --time=03:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner --mail-user=tina.shen@mail.utoronto.ca --mail-type=ALL VAEcontrast1.sh
+sbatch --nodes=1 --time=03:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner --mail-user=tina.shen@mail.utoronto.ca --mail-type=ALL DCE_VAE.sh
 
