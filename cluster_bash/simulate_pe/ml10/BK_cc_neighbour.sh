6cf5aa1b7082ab7742e22183abae2f97a2b756df
Tianshu Shen <tinashen.tianshu@gmail.com>
2022-05-05 21:40:37 -0400

    chore: renaming data folder for ml10 bash scripts

--- cluster_bash/simulate_pe/ml10/BK_cc_neighbour.sh
+++ cluster_bash/simulate_pe/ml10/BK_cc_neighbour.sh
@@ -5,4 +5,4 @@ source ~/vae_uncertainty/bin/activate
 cd /home/tinashen/projects/def-ssanner/tinashen/VAE_uncertainty_word_embeddings
 
 # Experiment 2. BK-NN
-python simulate.py --saved_model models_VAE/VAE_multilayer1.pt --data_name ml10_SIGIR --data_dir fold0 --conf sim_abs_diff_neg1_noise3_NN.config --top_items 15 --top_users 8000
+python simulate.py --saved_model models_VAE/VAE_multilayer1.pt --data_name ml10 --data_dir fold0 --conf sim_abs_diff_neg1_noise3_NN.config --top_items 15 --top_users 8000
--- cluster_bash/simulate_pe/ml10/DCE_normal.sh
+++ cluster_bash/simulate_pe/ml10/DCE_normal.sh
@@ -5,7 +5,7 @@ source ~/vae_uncertainty/bin/activate
 cd /home/tinashen/projects/def-ssanner/tinashen/VAE_uncertainty_word_embeddings
 
 # Experiment 1. DCE-normal
-python simulate.py --saved_model models_DCE-VAE/VAEcontrast1.pt --data_name ml10_SIGIR --data_dir fold0 --conf sim_abs_diff_neg1_noise10_normal.config --top_items 15 --top_users 8000
+python simulate.py --saved_model models_DCE-VAE/VAEcontrast1.pt --data_name ml10 --data_dir fold0 --conf sim_abs_diff_neg1_noise10_normal.config --top_items 15 --top_users 8000
 
 # Experiment 3. without clarification
-python simulate.py --saved_model models_DCE-VAE/VAEcontrast1.pt --data_name ml10_SIGIR --data_dir fold0 --conf sim_abs_diff_neg1_noise0_normal.config --top_items 15 --top_users 8000
+python simulate.py --saved_model models_DCE-VAE/VAEcontrast1.pt --data_name ml10 --data_dir fold0 --conf sim_abs_diff_neg1_noise0_normal.config --top_items 15 --top_users 8000
