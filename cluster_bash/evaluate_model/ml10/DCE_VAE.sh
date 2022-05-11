9a7dd19ac05cfbc926e57406b88557f94f1faac9
Tianshu Shen <tinashen.tianshu@gmail.com>
2022-05-11 15:12:21 -0400

    Update DCE_VAE.sh
    chore: format  model saving scripts format

--- cluster_bash/evaluate_model/ml10/DCE_VAE.sh
+++ cluster_bash/evaluate_model/ml10/DCE_VAE.sh
@@ -4,7 +4,7 @@ source ~/vae_uncertainty/bin/activate
 
 cd /home/tinashen/projects/def-ssanner/tinashen/VAE_uncertainty_word_embeddings
 
-python model_evaluate.py --model_name VAEmultilayer_contrast --data_name ml10_SIGIR --conf VAEcontrast1.config --log_dir VAEcontrast1 --top_items 15 --top_users 8000
+python model_evaluate.py --model_name VAEmultilayer_contrast --data_name ml10_SIGIR --conf VAEmultilayer_contrast1.config --log_dir VAEmultilayer_contrast1 --top_items 15 --top_users 8000
 
 
 
