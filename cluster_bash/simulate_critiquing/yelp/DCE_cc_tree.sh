9c7d5847532e7b8dbf466bc1d0f396152760d55a
Tianshu Shen <tinashen.tianshu@gmail.com>
2022-05-11 23:40:05 -0400

    Update DCE_cc_tree.sh
    chore: switch project directory on cc

--- cluster_bash/simulate_critiquing/yelp/DCE_cc_tree.sh
+++ cluster_bash/simulate_critiquing/yelp/DCE_cc_tree.sh
@@ -2,7 +2,7 @@
 module load python/3.7
 source ~/vae_uncertainty/bin/activate
 
-cd /home/tinashen/projects/def-ssanner/tinashen/VAE_uncertainty_word_embeddings
+cd /home/tinashen/projects/def-ssanner/tinashen/DCE-Clarification-Conversational-Critiquing
 
 # Experiment 2. DCE-Tree
 python simulate_yelp.py --saved_model models_DCE-VAE/VAEmultilayer_contrast3.pt --data_name yelp --data_dir fold0 --conf sim_abs_diff_neg100_noise0_distributional.config --top_items 10 --expNum Exp2_Clarification_Critique --methodName DCE-Tree
